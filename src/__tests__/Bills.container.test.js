279ddb0611324155684c79574fed87a887ffaabc
JonasPaturej <89771974+JonasPaturej@users.noreply.github.com>
2025-11-20 16:45:58 +0100

    Modification de /__tests__/Bills.js (Ajout de l'expect pour que l'icone soit active)
    Ajout de Bills.container.test.js (Ajout des tests unitaires pour /container/Bills.js, redirection vers NewBill, affichage de la modale, vérification du tri et mock du store)

---
+++ src/__tests__/Bills.container.test.js
@@ -0,0 +1,81 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import Bills from "../containers/Bills.js";
+import { ROUTES_PATH } from "../constants/routes.js";
+import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+import mockStore from "../__mocks__/store.js";
+
+jest.mock("../app/Store", () => mockStore);
+
+describe("Bills container", () => {
+  beforeEach(() => {
+    document.body.innerHTML = `
+      <div id="root"></div>
+      <button data-testid="btn-new-bill">New Bill</button>
+      <div data-testid="icon-eye" data-bill-url="https://test.fr/image.jpg"></div>
+      <div id="modaleFile"><div class="modal-body"></div></div>
+    `;
+
+    $.fn.modal = jest.fn();
+  });
+
+  test("handleClickNewBill should navigate to NewBill page", () => {
+    const onNavigate = jest.fn();
+    const bills = new Bills({
+      document,
+      onNavigate,
+      store: null,
+      localStorage: null,
+    });
+
+    const btn = screen.getByTestId("btn-new-bill");
+    userEvent.click(btn);
+
+    expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH.NewBill);
+  });
+
+  test("handleClickIconEye should open modal", () => {
+    const bills = new Bills({
+      document,
+      onNavigate: () => {},
+      store: null,
+      localStorage: null,
+    });
+
+    const icon = screen.getByTestId("icon-eye");
+    userEvent.click(icon);
+
+    expect($.fn.modal).toHaveBeenCalledWith("show");
+  });
+
+  test("getBills should return sorted & formatted bills", async () => {
+    const bills = new Bills({
+      document,
+      onNavigate: () => {},
+      store: mockStore,
+      localStorage: null,
+    });
+
+    const result = await bills.getBills();
+
+    expect(result.length).toBeGreaterThan(0);
+    expect(result[0].date).toMatch(/[A-Za-zé]+/);
+  });
+
+  test("should call store.bills().list()", async () => {
+    const spy = jest.spyOn(mockStore, "bills");
+
+    const bills = new Bills({
+      document,
+      onNavigate: () => {},
+      store: mockStore,
+      localStorage: null,
+    });
+    await bills.getBills();
+
+    expect(spy).toHaveBeenCalled();
+  });
+});
